7d80e14484961bb9a92fc312b5b1845ddac53d55
mrbatataisgyatted-maker <mrbatataisgyatted@gmail.com>
2026-09-21 14:51:30 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,7 @@ android {
         ndk {
             abiFilters.add("armeabi-v7a")
             abiFilters.add("arm64-v8a")
+            abiFilters.add("x86_64")
         }
 
         buildConfigField("Boolean", "RESET_ON_INSTALL", "true")
